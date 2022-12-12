32f1078a7922b08572a51f7820d248f1b047faea
Ismael M <jmorillolabour@gmail.com>
2022-12-12 16:09:27 +0100

    Added TextView

--- app/src/main/java/UF1/Threads/MainActivity.java
+++ app/src/main/java/UF1/Threads/MainActivity.java
@@ -2,9 +2,16 @@ package UF1.Threads;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,29 +20,36 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    String error = ""; // string field
+    String error = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
         Button b = findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getDataFromUrl("https://api.myip.com");
-            }
-        });
+        TextView txt = findViewById(R.id.textView);
+
+        b.setOnClickListener(view -> executor.execute(() -> {
+            String res = getDataFromUrl("https://api.myip.com");
+            Log.i("MYAPP",res);
+            handler.post(() -> txt.append(res));
+        }));
+
     }
 
     private String getDataFromUrl(String demoIdUrl) {
-
         String result = null;
         int resCode;
         InputStream in;
@@ -54,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 in = httpsConn.getInputStream();
 
                 BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        in, "iso-8859-1"), 8);
+                        in, StandardCharsets.ISO_8859_1), 8);
                 StringBuilder sb = new StringBuilder();
                 String line;
                 while ((line = reader.readLine()) != null) {
